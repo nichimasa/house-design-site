8ac22a357666df6ce0fc3cb5fab60b5b7c2e7874
masaya <nicchimasa@gmail.com>
2026-01-02 21:33:22 +0900

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     // Hugging Face Inference APIを呼び出し
     // 使用モデル: Stable Diffusion XL
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0',
+        'https://router.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0',
       {
         method: 'POST',
         headers: {
